70d5db31b8111611fff9cc4afe2de2c04cf413d6
Helder Paiva <helder.devjava@gmail.com>
2023-07-05 15:43:36 -0300

    laout pronto e add alguns estilos basicos

---
+++ main.js
@@ -0,0 +1,18 @@
+
+// array de livros
+let myLibrary = [];
+
+
+function Book() {
+  //the construtor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+function Loop(){
+
+}
+
+const a = 10;
